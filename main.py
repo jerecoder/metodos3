ced81151c181d4a216323ec1f352781bdb9ac825
Jeremias Figueiredo <jerefigueiredo@gmail.com>
2023-10-31 13:19:14 -0300

    funcion para conseguir ultimas d dimensiones

--- main.py
+++ main.py
@@ -6,47 +6,50 @@ import os
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
-        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):  # puedes agregar más extensiones si es necesario
+        if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
             img = Image.open(os.path.join(folder, filename))
             img_data = np.array(img)
             images.append(img_data)
     return images
 
 # Cargar todas las imágenes de la carpeta "dataset_imagenes"
-folder_path = "parapelotudear"
+folder_path = "dataset_imagenes"
 all_images = load_images_from_folder(folder_path)
 
-# Si quieres convertir todas las imágenes en vectores:
+# Convert all images to vectors
 image_vectors = [img.ravel() for img in all_images]
 
-# Para apilar todos los vectores en una matriz:
+# Stack all vectors into a matrix
 image_matrix = np.vstack(image_vectors)
 
-U, S, Vt = np.linalg.svd(image_matrix, )
+U, S, Vt = np.linalg.svd(image_matrix, full_matrices=False)
 
-print(S)
-
-def reduce_dimension(matrix, U, S, Vt, d):
+def dPrincipal(matrix, U, S, Vt, d):
     U_d = U[:, :d]
     S_d = np.diag(S[:d])
     Vt_d = Vt[:d, :]
-
     return np.dot(U_d, np.dot(S_d, Vt_d))
 
-d = min(image_matrix.shape[0], 12)  # Esto garantiza que d no sea mayor que el número de imágenes
-reduced_matrix = reduce_dimension(image_matrix, U, S, Vt, d)
+def dTail(matrix, U, S, Vt, d):
+    U_d = U[:, -d:]   # Get the last d columns
+    S_d = np.diag(S[-d:])  # Get the smallest d singular values
+    Vt_d = Vt[-d:, :]  # Get the last d rows
+    return np.dot(U_d, np.dot(S_d, Vt_d))
 
+
+d = min(image_matrix.shape[0], 16)  # Ensure d isn't larger than the number of images
+reduced_matrix = dTail(image_matrix, U, S, Vt, d)
 
 def reconstruct_image(vector, shape):
     return vector.reshape(shape)
 
-# Dimensiones originales de las imágenes
+# Original image dimensions
 original_shape = all_images[0].shape
 
-# Reconstruye las imágenes reducidas
+# Reconstruct the reduced images
 reconstructed_images = [reconstruct_image(row, original_shape) for row in reduced_matrix]
 
-# Muestra las imágenes originales y reconstruidas lado a lado
+# Display original and reconstructed images side by side
 for original, reconstructed in zip(all_images, reconstructed_images):
     plt.figure(figsize=(5, 2))
 
@@ -57,7 +60,7 @@ for original, reconstructed in zip(all_images, reconstructed_images):
 
     plt.subplot(1, 2, 2)
     plt.imshow(reconstructed, cmap='gray')
-    plt.title("Reconstruida")
+    plt.title("Reconstructed")
     plt.axis('off')
 
     plt.show()
