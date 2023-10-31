6b5acc7f65686bf85c926e05d6732a599e393278
Jeremias Figueiredo <jerefigueiredo@gmail.com>
2023-10-31 13:46:34 -0300

    added results of 1.2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def load_images_from_folder(folder):
             images.append(img_data)
     return images
 
-# Cargar todas las imágenes de la carpeta "dataset_imagenes"
+# Load all images from the folder "dataset_imagenes"
 folder_path = "dataset_imagenes"
 all_images = load_images_from_folder(folder_path)
 
@@ -24,20 +24,13 @@ image_matrix = np.vstack(image_vectors)
 
 U, S, Vt = np.linalg.svd(image_matrix, full_matrices=False)
 
-def dPrincipal(matrix, U, S, Vt, d):
-    U_d = U[:, :d]
-    S_d = np.diag(S[:d])
-    Vt_d = Vt[:d, :]
-    return np.dot(U_d, np.dot(S_d, Vt_d))
-
 def dTail(matrix, U, S, Vt, d):
-    U_d = U[:, -d:]   # Get the last d columns
-    S_d = np.diag(S[-d:])  # Get the smallest d singular values
-    Vt_d = Vt[-d:, :]  # Get the last d rows
+    U_d = U[:, -d:]
+    S_d = np.diag(S[-d:])
+    Vt_d = Vt[-d:, :]
     return np.dot(U_d, np.dot(S_d, Vt_d))
 
-
-d = min(image_matrix.shape[0], 16)  # Ensure d isn't larger than the number of images
+d = min(image_matrix.shape[0], 8)
 reduced_matrix = dTail(image_matrix, U, S, Vt, d)
 
 def reconstruct_image(vector, shape):
@@ -49,18 +42,41 @@ original_shape = all_images[0].shape
 # Reconstruct the reduced images
 reconstructed_images = [reconstruct_image(row, original_shape) for row in reduced_matrix]
 
-# Display original and reconstructed images side by side
-for original, reconstructed in zip(all_images, reconstructed_images):
-    plt.figure(figsize=(5, 2))
+# Display original and reconstructed images together in a big plot
+n = len(all_images)
+fig, axes = plt.subplots(n//4, 8, figsize=(20, 5*(n//4)))
 
-    plt.subplot(1, 2, 1)
-    plt.imshow(original, cmap='gray')
-    plt.title("Original")
-    plt.axis('off')
+for i in range(0, n, 4):
+    row_idx = i//4
+    # First set: Original and Reconstructed
+    axes[row_idx, 0].imshow(all_images[i], cmap='gray')
+    axes[row_idx, 0].axis('off')
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(reconstructed, cmap='gray')
-    plt.title("Reconstructed")
-    plt.axis('off')
+    axes[row_idx, 1].imshow(reconstructed_images[i], cmap='gray')
+    axes[row_idx, 1].axis('off')
 
-    plt.show()
+    # Second set: Original and Reconstructed
+    axes[row_idx, 2].imshow(all_images[i+1], cmap='gray')
+    axes[row_idx, 2].axis('off')
+
+    axes[row_idx, 3].imshow(reconstructed_images[i+1], cmap='gray')
+    axes[row_idx, 3].axis('off')
+
+    # Third set: Original and Reconstructed
+    axes[row_idx, 4].imshow(all_images[i+2], cmap='gray')
+    axes[row_idx, 4].axis('off')
+
+    axes[row_idx, 5].imshow(reconstructed_images[i+2], cmap='gray')
+    axes[row_idx, 5].axis('off')
+
+    # Fourth set: Original and Reconstructed
+    axes[row_idx, 6].imshow(all_images[i+3], cmap='gray')
+    axes[row_idx, 6].axis('off')
+
+    axes[row_idx, 7].imshow(reconstructed_images[i+3], cmap='gray')
+    axes[row_idx, 7].axis('off')
+
+# Adjust spacing between subplots
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.tight_layout()
+plt.show()
